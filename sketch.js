c51533df046a18e39126cd53b3097255745cac4d
Somil Budiyal <71014859+SomilBudiyal@users.noreply.github.com>
2020-09-23 22:56:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var deformation
 function setup() {
   createCanvas(1600,400);
   wall=createSprite(1500, 200, 30, height/2);
+
   car=createSprite(50,200,50,20);
   
   weight=random(400,1500);
@@ -17,6 +18,9 @@ function draw() {
   background("black");
    wall.shapeColor="grey"; 
    car.shapeColor="white"; 
+
+  // wall.depth = car.depth;
+   wall.depth = wall.depth + 1;
    
    if(wall.x-car.x < (car.width+wall.width)/2){
     car.velocityX=0;
@@ -27,6 +31,7 @@ function draw() {
     } 
    if(deformation<180 && deformation>100)
    {
+
      car.shapeColor="yellow";
    }
    if(deformation<100)
@@ -34,5 +39,6 @@ function draw() {
      car.shapeColor="green";
    } 
   }
+
   drawSprites();
 }
